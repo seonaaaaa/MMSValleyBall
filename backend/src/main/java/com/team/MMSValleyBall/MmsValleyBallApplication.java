4f8cb2ee99b34dcfe95237eb8af4c102a74ff535
kimhyell2 <107864473+kimhyell2@users.noreply.github.com>
2024-10-25 16:56:03 +0900

    AutoStatusSystem 기능 추가 (#37)

--- backend/src/main/java/com/team/MMSValleyBall/MmsValleyBallApplication.java
+++ backend/src/main/java/com/team/MMSValleyBall/MmsValleyBallApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
+@EnableScheduling
 public class MmsValleyBallApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MmsValleyBallApplication.class, args);
 	}
-
 }
--- backend/src/main/java/com/team/MMSValleyBall/repository/AutoStatusSystemRepository.java
+++ backend/src/main/java/com/team/MMSValleyBall/repository/AutoStatusSystemRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
+public interface AutoStatusSystemRepository extends JpaRepository<Payment, Long> {
     List<Payment> findByPaymentStatusAndPaymentCreateAtBefore(PaymentStatus paymentStatus, LocalDateTime threeDaysAgo);
 }
