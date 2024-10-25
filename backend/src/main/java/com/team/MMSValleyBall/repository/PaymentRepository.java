143575f3f94295ecd78c3dc995ca2b4da092c8c0
kimhyell2 <107864473+kimhyell2@users.noreply.github.com>
2024-10-25 16:06:46 +0900

    예원님 충돌 오류 수정 (#34)

---
+++ backend/src/main/java/com/team/MMSValleyBall/repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+package com.team.MMSValleyBall.repository;
+
+import com.team.MMSValleyBall.entity.Payment;
+import com.team.MMSValleyBall.enums.PaymentStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByPaymentStatusAndPaymentCreateAtBefore(PaymentStatus paymentStatus, LocalDateTime threeDaysAgo);
+}
--- backend/src/main/java/com/team/MMSValleyBall/repository/UserRepository.java
+++ backend/src/main/java/com/team/MMSValleyBall/repository/UserRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
-    Optional<Users> findByUserEmail(String userEmail);
-    Boolean existsByUserEmail(String userEmail);
+    // Users 객체를 직접 반환하는 메서드
     Users findByUserEmail(String userEmail);
+
+    // 이메일 존재 여부 확인 메서드
+    boolean existsByUserEmail(String userEmail);
 }
