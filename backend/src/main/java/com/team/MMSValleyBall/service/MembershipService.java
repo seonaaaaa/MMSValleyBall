c299dafef7ae69bd52189f8026fc5c247049e635
yeeeone <dpdnjs9808@gmail.com>
2024-10-25 12:39:05 +0900

    진짜 찐찐 수정 완 (#28)
    * 1. MoneyDTO 생성
    2. data.sql 수정
    
    * 1. MoneyDTO 생성
    2. data.sql 수정

--- backend/src/main/java/com/team/MMSValleyBall/service/MembershipService.java
+++ backend/src/main/java/com/team/MMSValleyBall/service/MembershipService.java
@@ -1,18 +1,22 @@
 package com.team.MMSValleyBall.service;
 
-import com.team.MMSValleyBall.repository.MembershipShipRepository;
-import com.team.MMSValleyBall.repository.UserRepository;
+import com.team.MMSValleyBall.dto.MembershipSalesDTO;
+import com.team.MMSValleyBall.entity.MembershipSales;
+import com.team.MMSValleyBall.repository.MembershipSalesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MembershipService {
-    private final MembershipShipRepository membershipRepository;
+    private final MembershipSalesRepository membershipRepository;
 
-    @Autowired
-    public MembershipService(MembershipShipRepository membershipRepository, UserRepository userRepository) {
+    public MembershipService(MembershipSalesRepository membershipRepository) {
         this.membershipRepository = membershipRepository;
     }
 
+    public void saveMembership(MembershipSalesDTO membership) {
+        //1. membershipSales 레코드 추가
+//        MembershipSales newMembership = new MembershipSales();
+//        newMembership.setMembershipSalesUser();
+    }
 }
-
