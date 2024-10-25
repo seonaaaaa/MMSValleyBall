374f0772d6acd1ce89b9a96b0d4fbd058621ce4c
yeeeone <dpdnjs9808@gmail.com>
2024-10-25 14:52:31 +0900

    1. 사용자 검색 시 userEmail을 이용하도록 통일함

--- backend/src/main/java/com/team/MMSValleyBall/repository/UserRepository.java
+++ backend/src/main/java/com/team/MMSValleyBall/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.team.MMSValleyBall.repository;
 import com.team.MMSValleyBall.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByUserEmail(String userEmail);
 }
