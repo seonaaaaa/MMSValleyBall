479cc7048722a2c3f9eb7f955109570fe3a470a0
seonaaaaa <seon04520@gmail.com>
2024-10-28 14:49:52 +0900

    - editProfile-page 수정 form 생성 (#47)
    -  signup-page 전화번호 입력 형식 변경
    
    Co-authored-by: kimhyell <hyelee9295@gmail.com>

--- frontend/src/components/mypage/DeleteAccount.vue
+++ frontend/src/components/mypage/DeleteAccount.vue
@@ -1,17 +1,36 @@
 <template>
-  <div class="players-page">
-    <!-- 상단 1차 메뉴 텍스트가 들어간 배경 박스 -->
-    <LogoHeader />
-  </div>
-</template>
-
-<script>
-import LogoHeader from '../common/LogoHeader.vue';
-
-export default {
-    name: 'DeleteAccount',
-    components: {
-        LogoHeader
+    <div class="delete-account-page">
+      <!-- 상단 1차 메뉴 텍스트가 들어간 배경 박스 -->
+      <LogoHeader />
+  
+      <!-- 예매 내역, 나의 멤버십, 개인정보 수정 -->
+      <div class="menu">
+        <div class="menu-item" :class="{ 'active-menu-item': activeMenu === '/mypage/reservations' }" @click="navigateTo('/mypage/reservations')">예매 내역</div>
+        <div class="menu-item" :class="{ 'active-menu-item': activeMenu === '/mypage/membership' }" @click="navigateTo('/mypage/membership')">나의 멤버십</div>
+        <div class="menu-item" :class="{ 'active-menu-item': activeMenu === '/mypage/edit-profile' }" @click="navigateTo('/mypage/edit-profile')">나의 정보 수정</div>
+      </div>
+    
+      <!-- 회원 탈퇴 페이지 내용 -->
+      <div class="delete-account-content">
+  
+  
+  
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import LogoHeader from '../common/LogoHeader.vue';
+  
+  export default {
+      name: 'DeleteAccount',
+      components: {
+      LogoHeader
+    },    
+    data() {
+      return {
+        activeMenu: this.$route.path // 현재 활성화된 경로
+      };
     },
 }
 </script>
