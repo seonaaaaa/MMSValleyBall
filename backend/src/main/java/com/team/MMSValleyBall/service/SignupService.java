73087af51e8ee32b5ccd5a89fa87e07ceb1b7560
kimhyell <hyelee9295@gmail.com>
2024-10-29 10:06:59 +0900

    crosMvc 파일 추가, service 수정 완료

--- backend/src/main/java/com/team/MMSValleyBall/service/SignupService.java
+++ backend/src/main/java/com/team/MMSValleyBall/service/SignupService.java
@@ -6,6 +6,7 @@ import com.team.MMSValleyBall.entity.Users;
 import com.team.MMSValleyBall.enums.UserRole;
 import com.team.MMSValleyBall.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import jakarta.persistence.EntityManager;
@@ -17,26 +18,26 @@ public class SignupService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @PersistenceContext
     private EntityManager em;
 
-    public String checkEmail(String UserEmail) {
+    public String checkEmail(String userEmail) {
         // 이메일 중복 확인
-        Boolean isUser = userRepository.existsByUserEmail(UserEmail);
-        if (isUser) {
-            return "이미 존재하는 이메일입니다.";
+        Boolean isUser = userRepository.existsByUserEmail(userEmail);
+        if(userEmail == ""){
+            return "null";
         }
-        return "사용 가능한 이메일입니다.";
+
+        if (isUser) {
+            return "False";
+        }
+        return "True";
     }
 
     public void signupProcess(UserDTO userDTO) {
-        // 기존 같은 유저가 있는지 확인
-        Boolean isUser = userRepository.existsByUserEmail(userDTO.getUserEmail());
-
-        if (isUser) {
-            throw new IllegalArgumentException("이미 존재하는 이메일입니다."); // 예외를 던져 중복 알림
-        }
-
         // 이번 시즌의 브론즈 멤버십 가져오기
         Long bronzeMembershipId = 2L;
         Membership bronzeMembership = em.find(Membership.class, bronzeMembershipId);
@@ -45,7 +46,9 @@ public class SignupService {
         // 없으면 회원가입
         Users data = new Users();
         data.setUserEmail(userDTO.getUserEmail());
-        data.setUserPassword(userDTO.getUserPassword());
+        data.setUserPassword(
+                bCryptPasswordEncoder.encode(userDTO.getUserPassword())
+        );
         data.setUserName(userDTO.getUserName());
         data.setUserPhone(userDTO.getUserPhone());
         data.setUserAddress(userDTO.getUserAddress());
