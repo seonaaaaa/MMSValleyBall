223dc21092813aa70cbf3b746177a0e2f4c716bf
13man <94jws@naver.com>
2024-11-03 09:51:54 +0900

    원상 끝 [adminPage] (#97)
    * home
    
    * 원상 끝

--- backend/src/main/java/com/team/MMSValleyBall/repository/UserRepository.java
+++ backend/src/main/java/com/team/MMSValleyBall/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package com.team.MMSValleyBall.repository;
 
 import com.team.MMSValleyBall.entity.Users;
+import com.team.MMSValleyBall.enums.UserRole;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
 
@@ -12,4 +18,14 @@ public interface UserRepository extends JpaRepository<Users, Long> {
     boolean existsByUserEmail(String userEmail);
 
     boolean existsByUserPhone(String userPhone);
+
+    Page<Users> findByUserNameContainingOrderByUserIdAsc(@Param("keyword") String keyword, Pageable pageable);
+
+    Page<Users> findByUserEmailContainingOrderByUserIdAsc(@Param("keyword") String keyword, Pageable pageable);
+
+    Page<Users> findByUserMembership_MembershipNameContainingIgnoreCaseOrderByUserIdAsc(@Param("keyword") String keyword, Pageable pageable);
+
+    List<Users> findByUserRole(UserRole userRole);
+
+    long countByUserRole(UserRole userRole);
 }
